087b03edfc923b5ef43a9041aba8dbce79d32017
yongchulbag <yongchulbag@gmail.com>
2022-06-10 23:27:20 +0900

    커밋

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -58,30 +58,3 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   }
 }
 
-class CommonButton extends StatelessWidget {
-  CommonButton({this.color, this.title, this.onPressed});
-
-  final Color color;
-  final String title;
-  final Function onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        elevation: 5.0,
-        color: color,
-        borderRadius: BorderRadius.circular(30.0),
-        child: MaterialButton(
-          onPressed: onPressed,
-          minWidth: 200.0,
-          height: 42.0,
-          child: Text(
-            title,
-          ),
-        ),
-      ),
-    );
-  }
-}
